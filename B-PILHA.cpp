b5d96448187c70b6eb682d7a59d9823ce1ee4dc2
Eduardo <coutoeduz@gmail.com>
2023-11-15 03:08:47 -0300

    Criando funções da pilha

--- B-PILHA.cpp
+++ B-PILHA.cpp
@@ -3,25 +3,68 @@
 using namespace std;
 
 
-typedef struct pilha {
+typedef struct no {
     int numero;
-    pilha* prox;
+    no* prox;
+} No;
+
+typedef struct pilha {
+    No* topo;
 } Pilha;
 
 Pilha* inicializa()
 {
-    return NULL;
+    No* topo = NULL;
 }
 
 Pilha* push(Pilha* p, int valor)
 {
-    Pilha* novo = new Pilha();
+    No* novo = new No();
     novo->numero = valor;
-    novo->prox = p;
-    return novo;
+    novo->prox = p->topo;
+    p->topo = novo;
 }
 
 void pop(Pilha* p)
 {
-    p = p->prox;
+    p->topo = p->topo->prox;
+}
+
+Pilha* listar(Pilha* p)
+{
+    No* aux = p->topo;
+    while(aux != NULL){
+        cout << aux->numero << "\n";
+        aux = aux->prox;
+    }
+}
+
+No* buscar(Pilha* p, int valor)
+{
+    No* aux = p->topo;
+    while(aux != NULL){
+        if(aux->numero == valor){
+            cout << "Número presente: "<< aux->numero << "\n";
+            return aux;
+        }
+        aux = aux->prox;
+    }
+
+    cout << "Número não encontrado na pilha!! \n";
+    return NULL;
+}
+
+int main()
+{
+    Pilha* p = new Pilha();
+    p = push(p, 7);
+    p = push(p, 8);
+    p = push(p, 12);
+    pop(p);
+
+    listar(p);
+
+    No* busca = buscar(p, 8);
+
+    return 0;
 }
